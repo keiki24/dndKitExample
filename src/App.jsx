3d964b024f38977913ec82d6f5e77e302cd4537f
Keita Sasaki <kabotyanopurin@gmail.com>
2024-02-10 22:22:32 +0900

    ドロップ対象のアイテムを複数にした

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,27 @@ import Droppable from "./Droppable.jsx";
 
 function App() {
   const containers = ["A", "B", "C"];
-  const [isDropped, setIsDropped] = useState(false);
-  const draggableMarkup = <Draggable>Drag me</Draggable>;
+  const [parent, setParent] = useState(null);
+  const draggableMarkup = <Draggable id="draggable">Drag me</Draggable>;
 
   return (
     <>
       <h1>dndKit</h1>
       <DndContext onDragEnd={handleDragEnd}>
-        {!isDropped ? draggableMarkup : null}
-        <Droppable>{isDropped ? draggableMarkup : "Drop here"}</Droppable>
+        {parent === null ? draggableMarkup : null}
+        {containers.map((id) => (
+          <Droppable key={id} id={id}>
+            {parent === id ? draggableMarkup : "Drop here"}
+          </Droppable>
+        ))}
       </DndContext>
     </>
   );
 
   function handleDragEnd(event) {
-    if (event.over && event.over.id === "droppable") {
-      setIsDropped(true);
-    }
+    const { over } = event;
+
+    setParent(over ? over.id : null);
   }
 }
 
--- src/Draggable.jsx
+++ src/Draggable.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import {useDraggable} from '@dnd-kit/core';
+import React from "react";
+import { useDraggable } from "@dnd-kit/core";
 
 function Draggable(props) {
-  const {attributes, listeners, setNodeRef, transform} = useDraggable({
-    id: 'draggable',
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: props.id,
   });
-  const style = transform ? {
-    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-  } : undefined;
+  const style = {
+    ...(transform
+      ? {
+          transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+        }
+      : {}),
+    width: "100px",
+    height: "20px",
+    fontSize: "14px",
+  };
 
-  
   return (
     <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
       {props.children}
