ded4887c71c68c156d6946e8d16a2315eb86588e
Keita Sasaki <kabotyanopurin@gmail.com>
2024-05-13 20:36:24 +0900

    ドロップアイテムの追加と削除のためのボタンを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { DndContext, MouseSensor, useSensor, useSensors } from "@dnd-kit/core";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Draggable from "./Draggable.jsx";
 import Droppable from "./Droppable.jsx";
 
 import "./main.css";
 
 function App() {
-
   // droppable item list
   const containers = [
     "droppable-1",
@@ -31,6 +31,35 @@ function App() {
     "droppable-4": false,
   });
 
+  // draggable item を追加する関数
+  const addDraggable = () => {
+    if (draggables.length < 10) {
+      const newDraggable = {
+        id: uuidv4(),
+        content: `Drag me ${draggables.length + 1}`,
+      };
+      setDraggables((prevDraggables) => [...prevDraggables, newDraggable]);
+    } else {
+      console.log("Maximum number of draggable items reached.");
+    }
+  };
+
+  const deleteDraggable = () => {
+    setDraggables((prevDraggables) => {
+      const sortedDraggables = [...prevDraggables].sort((a, b) => {
+        if (a.content > b.content) {
+          return -1;
+        } else if (a.content < b.content) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+      const newDraggables = sortedDraggables.slice(1, sortedDraggables.length);
+      return newDraggables;
+    });
+  };
+
   // draggable item のリスト
   const [draggables, setDraggables] = useState([
     { id: "draggable-1", content: "Drag me 1" },
@@ -50,8 +79,19 @@ function App() {
   return (
     <>
       <h1 className="text-lg font-bold">dnd-Kit Example</h1>
+      <button
+        onClick={addDraggable}
+        className="mx-1 my-1 rounded-md bg-blue-700 px-10 py-1 text-white hover:bg-blue-800"
+      >
+        Add
+      </button>
+      <button
+        onClick={deleteDraggable}
+        className="mx-1 my-1 rounded-md bg-red-700 px-10 py-1 text-white hover:bg-red-800"
+      >
+        Delete
+      </button>
       <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
-        {/* ドラッグ元 */}
         <div>
           {draggables
             .filter((drag) => !Object.values(droppables).includes(drag.id))
@@ -93,12 +133,15 @@ function App() {
     if (over) {
       console.log(event);
       setDroppables((prevDroppable) => {
-        const newDroppables = Object.keys(prevDroppable).reduce((accumulator, key) => {
-          // ドラッグ&ドロップ元からドラッグ要素を削除する
-          accumulator[key] =
-            prevDroppable[key] === active.id ? null : prevDroppable[key];
-          return accumulator;
-        }, {});
+        const newDroppables = Object.keys(prevDroppable).reduce(
+          (accumulator, key) => {
+            // ドラッグ&ドロップ元からドラッグ要素を削除する
+            accumulator[key] =
+              prevDroppable[key] === active.id ? null : prevDroppable[key];
+            return accumulator;
+          },
+          {},
+        );
 
         // 新しいドラッグ&ドロップ先にドラッグ要素を設定する
         newDroppables[over.id] = active.id;
